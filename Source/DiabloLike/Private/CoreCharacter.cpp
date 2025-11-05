a27d07145243b4ea8abe9008af00fcefd7461701
SparkiiX987 <150683820+SparkiiX987@users.noreply.github.com>
2025-11-05 13:29:48 +0100

    Create CoreCharacter + C_Player + add player player movements

---
+++ Source/DiabloLike/Private/CoreCharacter.cpp
@@ -0,0 +1,76 @@
+#include "CoreCharacter.h"
+
+ACoreCharacter::ACoreCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACoreCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACoreCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACoreCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ACoreCharacter::TakeDamages(float _amount)
+{
+	currentHealth -= _amount;
+	if (currentHealth <= 0)
+	{
+		bIsDead = true;
+		OnDeath();
+	}
+}
+
+void ACoreCharacter::Heal(float _amount)
+{
+	if (currentHealth == maxHealth) { return; }
+
+	currentHealth = currentHealth + _amount > maxHealth ? maxHealth : currentHealth + _amount;
+}
+
+void ACoreCharacter::Revive()
+{
+	if (!bIsDead) { return; }
+
+	currentHealth = maxHealth * 1.25f;
+	bIsDead = false;
+}
+
+void ACoreCharacter::ModifyHealth(float _amount)
+{
+	maxHealth += _amount;
+}
+
+void ACoreCharacter::ModifyDamages(float _amount)
+{
+	attackDamage += _amount;
+}
+
+void ACoreCharacter::ModifyArmor(float _amount)
+{
+	armor += _amount;
+}
+
+void ACoreCharacter::ModifyAttackSpeed(float _amount)
+{
+	attackSpeed += _amount;
+}
+
+void ACoreCharacter::ModifyMovementSpeed(float _amount)
+{
+	movementSpeed += _amount;
+}
+
